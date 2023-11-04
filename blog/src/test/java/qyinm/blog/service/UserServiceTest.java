a6dcad56986b6ecca534194f8dda80696d8c6278
seungwoo byeon <mongoose@yu.ac.kr>
2023-11-04 09:21:52 +0000

    test: user signup success, fail testing

--- blog/src/test/java/qyinm/blog/service/UserServiceTest.java
+++ blog/src/test/java/qyinm/blog/service/UserServiceTest.java
@@ -2,10 +2,13 @@ package qyinm.blog.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import qyinm.blog.domain.User.UserRepository;
 import qyinm.blog.dto.UserDto;
 
 @SpringBootTest
@@ -14,10 +17,38 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void cleanup() {
+        userRepository.deleteAll();
+    }
+
     @Test
     void 회원가입_OK() {
-        UserDto userDto = new UserDto("abcd", "abcd", "abcd");
+        UserDto userDto = UserDto.builder()
+                .email("test@test.com")
+                .password("test")
+                .build();
 
         assertThat(userService.signup(userDto).getEmail()).isEqualTo(userDto.getEmail());
+    }
+
+    @DisplayName("회원가입 중복 email")
+    @Test
+    void 회원가입_fail() {
+        UserDto userDto = UserDto.builder()
+                .email("test@test.com")
+                .password("test")
+                .build();
+        userService.signup(userDto);
+
+        UserDto errDto = UserDto.builder()
+                .email("test@test.com")
+                .password("error")
+                .build();
+
+        assertThatIllegalArgumentException().isThrownBy(() -> userService.signup(errDto));
     }
 }
